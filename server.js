59ab1071cef53435650122d80676e2bca12ebd8e
Damian Padilla <damipad4@gmail.com>
2023-01-16 01:41:37 -0600

    added body parser

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const PORT = 3000
 
 
-//========
+//===========
+// Middleware
+//===========
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+//======
 //Routes
-//========
+//======
 app.get('/', (req,res) =>{
     res.sendFile(__dirname + "/index.html")
 })
 
-//=========
+app.post('/quotes', (req, res) => {
+    console.log(req.body);
+})
+
+//========
 //Listener
-//=========
+//========
 app.listen(PORT, function() {
     console.log(`Listening on port ${PORT}`);
 })
