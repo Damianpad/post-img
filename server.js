6c67d78906599aa00869434d40c074c07ec67642
Damian Padilla <damipad4@gmail.com>
2023-01-16 17:58:02 -0600

    connected db

--- server.js
+++ server.js
@@ -2,24 +2,39 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const PORT = 3000
+const MongoClient = require('mongodb').MongoClient
+const connectionString = 'mongodb+srv://img-post:g7WXzbqFWl9MsnwF@cluster0.ainxlwh.mongodb.net/?retryWrites=true&w=majority'
+//g7WXzbqFWl9MsnwF
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database');
+        const db = client.db('post-img')
+
+        //===========
+        // Middleware
+        //===========
+        app.use(bodyParser.urlencoded({ extended: true }))
+    
+        //======
+        //Routes
+        //======
+        app.get('/', (req,res) =>{
+            res.sendFile(__dirname + "/index.html")
+        })
+
+        app.post('/quotes', (req, res) => {
+        console.log(req.body);
+        })
+
+    })
+    
+    .catch(error => console.error(error))
 
 
-//===========
-// Middleware
-//===========
-app.use(bodyParser.urlencoded({ extended: true }))
 
 
-//======
-//Routes
-//======
-app.get('/', (req,res) =>{
-    res.sendFile(__dirname + "/index.html")
-})
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body);
-})
 
 //========
 //Listener
